525636b9f61e7dc4d0e958854d5ddbe2d2a15b2a
Dawid Skowroński <dawidds123@gmail.com>
2024-09-22 11:05:47 +0200

    Redone movement, added working collisions, adjusted anim
    There is a completely new script for movement which supports collision detections, adjusted animations and position of the frog

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,57 +4,99 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveOffset = 1f;
-    public Animator anim;
+    Vector3 up = Vector3.zero,
+    right = new Vector3(0, 90, 0),
+    down = new Vector3(0, 180, 0),
+    left = new Vector3(0, 270, 0),
+    currentDirection = Vector3.zero;
 
-   
+    Vector3 nextPos, destination, direction;
+
+    public Animator anim;
+    float speed = 5f;
+    float rayLength = 1f;
+
+    bool canMove;
+    bool hasMovedAnim;
+
+    void Start()
+    {
+        currentDirection = up;
+        nextPos = Vector3.forward;
+        destination = transform.position;
+    }
 
     void Update()
     {
-    float xOffset = 0;
-    float zOffset = 0;
+        hasMovedAnim = false;
+        
+        Move();
 
-    bool hasMoved = false;
-    bool hasMovedAnim = false;
-
-    if ((Input.GetKeyDown(KeyCode.A)) || Input.GetKeyDown(KeyCode.LeftArrow))
-    {
-        xOffset = -moveOffset;
-        hasMoved = true;
-        transform.rotation = Quaternion.identity;
-        transform.rotation *= Quaternion.Euler(0f, -90f, 0f);
-    }
-    if ((Input.GetKeyDown(KeyCode.D)) || Input.GetKeyDown(KeyCode.RightArrow))
-    {
-        xOffset = moveOffset;
-        hasMoved = true;
-        transform.rotation = Quaternion.identity;
-        transform.rotation *=Quaternion.Euler(0f, 90f, 0f);
-    }
-     if ((Input.GetKeyDown(KeyCode.W)) || Input.GetKeyDown(KeyCode.UpArrow))
-    {
-        zOffset = moveOffset;
-        hasMoved = true;
-        transform.rotation = Quaternion.identity;
-        transform.rotation *=Quaternion.Euler(0f, 0f, 0f);
-    }
-    if ((Input.GetKeyDown(KeyCode.S)) || Input.GetKeyDown(KeyCode.DownArrow))
-    {
-        zOffset = -moveOffset;
-        hasMoved = true;
-        transform.rotation = Quaternion.identity;
-        transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
-    }
-    if (hasMoved == true)
-    {
-        Vector3 curPos = gameObject.transform.position;
-        gameObject.transform.position = new Vector3(curPos.x + xOffset, curPos.y, curPos.z + zOffset);
-
-        hasMovedAnim = true;
+       
+      
+        anim.SetBool("hasMovedAnim", hasMovedAnim);
     }
 
-    anim.SetBool("hasMovedAnim", hasMovedAnim);
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, speed *Time.deltaTime);
 
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            nextPos = Vector3.forward;
+            currentDirection = up;
+            canMove = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            nextPos = Vector3.back;
+            currentDirection = down;
+            canMove = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            nextPos = Vector3.left;
+            currentDirection = left;
+            canMove = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            nextPos = Vector3.right;
+            currentDirection = right;
+            canMove = true;
+        }
 
+        if (Vector3.Distance(destination, transform.position) <= 0.00001f)
+        {
+            transform.localEulerAngles = currentDirection;
+            if(canMove)
+            {
+                if(Valid())
+                {
+                    destination = transform.position + nextPos;
+                    direction = nextPos;
+                    canMove = false;
+                    hasMovedAnim = true;
+                }
+                
+            }
+        }
+    }
+
+    bool Valid()
+    {
+        Ray myRay = new Ray(transform.position + new Vector3(0, 0.25f, 0), transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(myRay.origin, myRay.direction, Color.red);
+
+        if(Physics.Raycast(myRay, out hit, rayLength))
+        {
+            if(hit.collider.tag == "Wall")
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
