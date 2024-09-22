9bb056207e3e8504b5dd04ff3fa25fe86a68de0f
HohiMohi <120066445+HohiMohi@users.noreply.github.com>
2024-09-22 17:12:45 +0200

    Revert "Merge branch 'main' of https://github.com/HohiMohi/HelloGJ"
    This reverts commit 9e0377b3b0eca70eafc058a707909408d6c67fa5, reversing
    changes made to 3b8b84380bee6b094e4b387a8e4f20e19a34a26b.

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -22,7 +22,7 @@ public class PauseController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.H))
         {
             if (_isPaused == false && _isPauseAvailable == true)
             {
